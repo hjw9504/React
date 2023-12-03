e11c0d5060085ff2669f9959038f85ca2b5d7af8
hjw9504 <hjw9504@naver.com>
2023-12-03 20:54:12 +0900

    login check test

--- src/component/MyPage.tsx
+++ src/component/MyPage.tsx
@@ -1,61 +1,71 @@
 import axios from "axios";
-import { useEffect, useLayoutEffect, useState } from "react";
-import { Navigate } from "react-router-dom";
+import {useEffect, useLayoutEffect, useState} from "react";
+import {Navigate} from "react-router-dom";
 
 export default function MyPage() {
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
   const [data, setData] = useState(null);
   const [input, setInput] = useState("");
 
   useLayoutEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts/1")
-    .then(response => response.json())
-    .then(response => {
-      setData(response);
-    })
+      .then((response) => response.json())
+      .then((response) => {
+        setData(response);
+      });
 
-      setIsLogin(true);
-      console.log(isLogin);
+    checkLogin();
+    console.log("useLayoutEffect: ", isLogin);
   }, []);
 
   useEffect(() => {
-    // return <Navigate to="/login" replace={true} />;
-    setIsLogin(false);
-  })
+    checkLogin();
+  });
 
-  async function onClick() {
-    try {
-      const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${input}`)
-      const response = await res.json();
-      setData(response);
-      console.log(isLogin);
+  const checkLogin = () => {
+    console.log("Login: ", isLogin);
+    console.log("Input: ", input);
+    if (input === "5") {
+      setIsLogin(false);
     }
-    catch(err) {
+  };
+
+  const onClick = async () => {
+    try {
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${input}`
+      );
+      const response = await res.json();
+      await onHandleData(response);
+    } catch (err) {
       console.log(err);
     }
+  };
+
+  const onHandleData = async (response: any) => {
+    setData(response);
+  };
+
+  if (!isLogin) {
+    return <Navigate to="/login" replace={true} />;
   }
-  
+
   return (
     <div>
       <h1>MyPage</h1>
       <p>MyPage 페이지입니다.</p>
       <div>
         <input
-        value = {input}
-        onChange = {(e)=>{
+          value={input}
+          onChange={(e) => {
             setInput(e.target.value);
-        }}/>
+          }}
+        />
       </div>
       <div>
         <button onClick={onClick}>불러오기</button>
       </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={data['title']}
-          readOnly={true}
-        />
-      )}
+      {data && <textarea rows={7} value={data["title"]} readOnly={true} />}
     </div>
   );
 }
