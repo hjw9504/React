a5a6b68794c3d95051e7de6e6236e0f39296b723
hjw9504 <hjw9504@naver.com>
2024-01-28 17:40:50 +0900

    register page update

--- src/component/Register.tsx
+++ src/component/Register.tsx
@@ -34,6 +34,27 @@ export default function Home() {
     setEmail(event.target.value);
   };
 
+  const checkUserId = async () => {
+    if (userId === undefined || userId === null || userId === "") {
+      alert("Input UserId");
+      return;
+    }
+
+    await fetch(`/check/userId?userId=${userId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res) {
+          alert("Already Exist UserId");
+          setUserId("");
+        }
+      });
+  };
+
   const onSignUp = async () => {
     try {
       const data = {
@@ -60,6 +81,10 @@ export default function Home() {
     }
   };
 
+  const goSignIn = () => {
+    navigate("/login");
+  };
+
   return (
     <>
       {/*
@@ -84,21 +109,29 @@ export default function Home() {
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <div>
-            <label
-              htmlFor="userId"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              ID
-            </label>
-            <div className="mt-2 my-2">
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="userId"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                ID
+              </label>
+            </div>
+            <div className="flex mt-2 my-2">
               <input
                 id="userId"
                 name="userId"
                 onChange={saveUserId}
                 type="text"
                 required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-[70%] rounded-md border-0 mr-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
+              <button
+                onClick={checkUserId}
+                className="flex w-[30%] justify-center items-center rounded-md bg-indigo-600 px-3 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Check ID
+              </button>
             </div>
           </div>
 
@@ -168,12 +201,18 @@ export default function Home() {
             </div>
           </div>
 
-          <div>
+          <div className="flex justify-center">
             <button
               onClick={onSignUp}
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="w-[160px] justify-center rounded-md bg-indigo-600 py-1.5 mr-10 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Register
+            </button>
+            <button
+              onClick={goSignIn}
+              className="w-[160px] justify-center rounded-md bg-indigo-600 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign In
             </button>
           </div>
         </div>
